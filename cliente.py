76ac21e23752db90db97904e794a22513f800209
VitorAndradeSchweitzer <vitor2.a.schweitzer@gmail.com>
2024-09-29 11:11:34 -0300

    emplementando cliente.pedir_musica

--- cliente.py
+++ cliente.py
@@ -1,5 +1,6 @@
-from fila import Fila_Karaoke, Fila
+from fila import Fila
 from musica import Musica
+from bibliotecademusicas import BibliotecaDeMusicas
 class Cliente:
     def __init__(self, nome):
         self.__nome = nome
@@ -30,9 +31,30 @@ class Cliente:
     def prioridade_fila(self, prioridade_fila: int):
         self.__prioridade_fila = prioridade_fila
 
-    def pedir_musica(self, musica: Musica, fila: Fila):
+    def pedir_musica(self, biblioteca_de_musicas: BibliotecaDeMusicas, fila: Fila):
+        opcao_musica = int(input("Caso cliente queira buscar a música digite 1, caso queira adicionar música digite 2:"))
+        if opcao_musica == 1:
+            buscar_criterio = int(input("Gostaria de buscar música por: Gênero[1], Artista[2] ou Idioma[3]?"))
+            
+            if buscar_criterio == 1:
+                criterio = "Gênero"
+            elif buscar_criterio == 2:
+                criterio = "Artista"
+            else:
+                criterio = "Idioma"
+            
+            buscar_filtro = str(input("Nome da música (caso não queira filtrar por nome, deixe em branco):"))
+            
+            musica = biblioteca_de_musicas.buscar_musica(criterio, buscar_filtro)
+        
+        else:
+            musica = biblioteca_de_musicas.adicionar_musica()
+
+            
+        
+        
         novo_pedido = [self, musica]
         fila.adcionar_na_fila(novo_pedido)
     
     def remover_da_fila(self, musica: Musica, fila: Fila):
-        
+        pass
